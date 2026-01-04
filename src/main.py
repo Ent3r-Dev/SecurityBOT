dce27e9409fe77eaff3767cfe0cae3e8b7edd8fd
KaiTomotake <kaitomotake@gmail.com>
2026-01-05 00:35:03 +0900

    banコマンドと関連する関数などを追加
    - banコマンドを追加
    - 内部的な便利関数 `create_report`を追加。今後はこの関数を使ってエラー報告が可能
    - 内部的な便利関数 `create_log`を追加。今後はこの関数を使用して成功報告が可能。
    - BAN日時を表示するためにdatetimeをインポート

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
 import dotenv
+import datetime
 
 import pathlib
 
@@ -8,11 +9,67 @@ class Client(discord.Client):
         intents = discord.Intents.none()
         super().__init__(intents=intents)
         self.tree = discord.app_commands.CommandTree(self)
-    
+
     async def setup_hook(self):
         await self.tree.sync()
-    
+
+def create_report(title: str) -> discord.Embed:
+    embed = discord.Embed(title=title, timestamp=datetime.datetime.now())
+    embed.set_footer(text="SecurityBOT | ErrorReport")
+
+
+def create_log(title: str) -> discord.Embed:
+    embed = discord.Embed(title=title, timestamp=datetime.datetime.now())
+    embed.set_footer(text="SecurityBot | Action Log")
+
+
+@discord.app_commands.command(name="ban", description="メンバーをBANします")
+@discord.app_commands.guild_only
+@discord.app_commands.default_permissions(ban_members=True)
+@discord.app_commands.describe(
+    target="BANするメンバー", reason="理由", delete="メッセージを削除する日数"
+)
+async def ban_cmd(
+    interaction: discord.Interaction, target: discord.Member, reason: str, delete: int
+):
+    try:
+        await target.ban(delete_message_days=delete, reason=reason)
+    except discord.Forbidden:
+        embed = create_report("BOTの権限が足りません")
+        embed.add_field(
+            name="対象", value=f"{target.name} (`{target.id}`)", inline=False
+        )
+        embed.add_field(
+            name="実行者",
+            value=f"{interaction.user.name} (`{interaction.user.id}`)",
+            inline=False,
+        )
+    except Exception as e:
+        embed = create_report("BANに失敗しました")
+        embed.add_field(
+            name="対象", value=f"{target.name} (`{target.id}`)", inline=False
+        )
+        embed.add_field(
+            name="実行者",
+            value=f"{interaction.user.name} (`{interaction.user.id}`)",
+            inline=False,
+        )
+        embed.add_field(name="エラー", value=type(e).__name__, inline=False)
+    else:
+        embed = create_log("BANに成功しました")
+        embed.add_field(
+            name="対象", value=f"{target.name} (`{target.id}`)", inline=False
+        )
+        embed.add_field(
+            name="実行者",
+            value=f"{interaction.user.name} (`{interaction.user.id}`)",
+            inline=False,
+        )
+        embed.add_field(name="理由", value=reason, inline=False)
+    await interaction.response.send_message(embed=embed)
+
 if __name__ == "__main__":
+    client = Client()
     dotenv_path = pathlib.Path(__file__).parent.joinpath(".env")
     token = dotenv.get_key(dotenv_path, "DISCORD_TOKEN")
-    Client().run(token)
+    client.run(token)
